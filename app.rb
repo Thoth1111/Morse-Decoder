44a40b9a267eb40355f946e4dd7d7170e422aaa2
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-05 15:57:49 +0300

    Create method to decode char and word

--- app.rb
+++ app.rb
@@ -1,43 +1,44 @@
-@alphabets = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--"  => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z",
-};
+# frozen_string_literal: true
 
-def decode_char (string)
-    @alphabets[string]
-  end
+@alphabets = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(string)
+  @alphabets[string]
+end
 
 def decode_word(string)
-    @text = []
-    each = string.split(' ')
-    each.each do |i|
-       @newWord = decode_char(i)
-       @text.push(@newWord)
-    end
-    print "#{@text.join()} "
-   end
-  
+  @text = []
+  each = string.split(' ')
+  each.each do |i|
+    @new_word = decode_char(i)
+    @text.push(@new_word)
+  end
+  print "#{@text.join} "
+end
